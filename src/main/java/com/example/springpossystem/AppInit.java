f246c90fc6724c773b35133aeea779e1f186eefe
chamika-damith <chamikadamith9@gmail.com>
2024-10-07 14:01:01 +0530

    config configuration

---
+++ src/main/java/com/example/springpossystem/AppInit.java
@@ -0,0 +1,25 @@
+package com.example.springpossystem;
+
+
+import com.example.springpossystem.config.WebAppConfig;
+import com.example.springpossystem.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
---
+++ src/main/java/com/example/springpossystem/config/WebAppConfig.java
@@ -0,0 +1,14 @@
+package com.example.springpossystem.config;
+
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Component
+@ComponentScan(basePackages = "org.example.springpos")
+@EnableWebMvc
+@MultipartConfig
+public class WebAppConfig {
+}
+
